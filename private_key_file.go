4871b2647156ca3a29cfba13f74eaf0909befe95
Doug Breshears <doug@jshfarms.com>
2019-07-09 08:03:38 -0700

    Add notes for PrivateKey

--- private_key_file.go
+++ private_key_file.go
@@ -19,7 +19,8 @@ func PrivateKeyFile(file string) ssh.AuthMethod {
 	return ssh.PublicKeys(key)
 }
 
-
+// Allow the user to get the keyfile contents in whatever manner they
+// want. e.g. go-bindata for embedded assets
 func PrivateKey(key_buffer []byte) ssh.AuthMethod {
 	key, err := ssh.ParsePrivateKey(key_buffer)
 	if err != nil {
